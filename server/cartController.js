f86f57ab7f1566e58d0510c0e7f0a95a1a207394
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2023-12-19 22:42:13 -0500

    fix cart controller errors

--- server/cartController.js
+++ server/cartController.js
@@ -4,6 +4,7 @@ const Sequelize = require("sequelize");
 const { QueryTypes } = require('sequelize');
 const bcrypt = require("bcryptjs");
 const { default: Stripe } = require("stripe");
+const { useId } = require("react");
 const stripe = require('stripe')(process.env.STRIPE_SECRET_TEST)
 
 // you wouldn't want to rejectUnauthorized in a production app, but it's great for practice
@@ -27,24 +28,26 @@ module.exports = {
   },
   
   addToCart: (req, res) => {
-    let { user_id, item_id, quantity } = req.body;
+    let { user_id, item_id, quanity } = req.body;
     user_id = parseInt(user_id);
+    console.log(req.body)
   
     sequelize
       .query(
-        'INSERT INTO cart (user_id, item_id, quantity) VALUES (:user_id, :item_id, :quantity);',
+        'INSERT INTO cart (user_id, item_id, quanity) VALUES (:user_id, :item_id, :quanity);',
         {
-          replacements: { user_id, item_id, quantity },
-          type: QueryTypes.SELECT
+          replacements: { user_id, item_id, quanity },
+          type: QueryTypes.INSERT
         }
       )
-      .then((dbRes) => {
-        return res.status(200).send(dbRes[0]);
+      .then(() => {
+        return res.sendStatus(200); 
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
-      });
-  },
+      }
+      )
+},
   
   getCart: (req, res) => {
     const user_id = parseInt(req.params.user_id);
@@ -90,19 +93,14 @@ module.exports = {
       });
   },
   
-  cartNumber: (req, res) => {
-    const id = parseInt(req.params.id);
-  
-    sequelize
-      .query('SELECT COUNT(item_id) FROM cart WHERE user_id = :id;', {
-        replacements: { id },
-        type: QueryTypes.SELECT
-      })
-      .then((dbRes) => {
-        const cartNum = parseInt(dbRes[0].count);
-        return res.status(200).send(cartNum);
+  cartNumber: (req,res)=>{
+    const id =  parseInt(req.params.id)
+    sequelize.query(`SELECT COUNT(item_id)FROM cart WHERE user_id = ${id};`).then((dbRes)=>{
+      const cartNum = parseInt(dbRes[0][0].count)
+      return res.status(200).send(dbRes[0][0].count)
       });
-  },
+  }
+,
   
   getCartTotal: (req, res) => {
     const id = parseInt(req.params.id);
